aa8ad8ca6bb2099bbffff1a07e2bb405038dfabd
AmitM0287 <amitmanna0287@gmail.com>
2021-08-03 11:34:58 +0530

    feat: urls added

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from auth_app import views
 
-
 # URL Configuration for auth_app
 app_name='auth_app'
 
 urlpatterns = [
     path('login/', views.LoginAPIView.as_view(), name='user_login'),
     path('register/', views.RegisterAPIView.as_view(), name='user_register'),
+    path('reset-password/<token>', views.ResetPasswordAPIView.as_view(), name='user_reset_password'),
+    path('forgot-password/', views.ForgotPasswordAPIView.as_view(), name='user_forgot_password'),
 ]
