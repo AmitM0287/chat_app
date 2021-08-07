fc897d0ecd3ab27d588166f58504f9e10d57cc85
AmitM0287 <amitmanna0287@gmail.com>
2021-08-07 10:58:05 +0530

    refactor: urls updated

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
     path('user/', include('auth_app.urls', namespace='auth_app')),
+    path('', include('chat.urls')),
 ]
